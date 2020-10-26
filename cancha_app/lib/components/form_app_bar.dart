b417db486bf3fa27106b2eabce8e5eb5618f6c6e
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2020-10-25 20:11:05 -0400

    Customizes the Form AppBar

--- cancha_app/lib/components/form_app_bar.dart
+++ cancha_app/lib/components/form_app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class FormAppBar extends StatelessWidget with PreferredSizeWidget {
   const FormAppBar({Key key}) : super(key: key);
@@ -8,23 +9,39 @@ class FormAppBar extends StatelessWidget with PreferredSizeWidget {
     return AppBar(
       toolbarHeight: 150,
       automaticallyImplyLeading: false,
-      title: Center(
-        child: Text(
-          "Nueva Reserva",
-          maxLines: 2,
-          style: TextStyle(
-              fontFamily: "Montserrat",
-              fontSize: 42,
-              fontWeight: FontWeight.bold,
-              color: Colors.white),
+      title: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Icon(
+          Icons.arrow_back,
+          color: Colors.white,
+          size: 30,
         ),
-      ),
+        Row(mainAxisSize: MainAxisSize.min, children: [
+          SvgPicture.asset(
+            "lib/resources/images/icons/ball.svg",
+            height: 100,
+            width: 100,
+            color: Color(0xFFB57402),
+          ),
+          Flexible(
+            child: Text(
+              "Nueva Reserva",
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(
+                  fontFamily: "Montserrat",
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+            ),
+          ),
+        ]),
+      ]),
       backgroundColor: Color(0xFFFFA901),
       shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(bottomRight: Radius.circular(35))),
+          borderRadius: BorderRadius.only(bottomRight: Radius.circular(50))),
     );
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(150);
+  Size get preferredSize => Size.fromHeight(165);
 }
